7c2d044d1bbb9f732a3491f61829b82f21da75a3
Eugene Strigo <e.strigo@onlinepay.com>
2019-01-24 17:08:59 +0300

    PTH-238 #time 30m #comment Adding landing more gently

--- app/actions/add-landing.js
+++ app/actions/add-landing.js
@@ -8,17 +8,24 @@ const updateLandingData = require('./helpers/update-landing-data');
 module.exports = async (ctx, next) => {
 
     const body = ctx.request.body || {};
+    const name = (body.name || '').trim();
+    const landingUpdate = body.landing;
 
-    const data = updateLandingData(ctx, {}, {
-        name: (body.name || '').trim(),
-        landing: body.landing
-    });
+    const update = {};
+    if (name) {
+        update.name = name;
+    }
+    if (landingUpdate && !_.isEmpty(landingUpdate)) {
+        update.landing = landingUpdate;
+    }
 
-    if (!data.name) {
+    if (_.isEmpty(update)) {
         const err = new Error('Bad request');
         err.status = 400;
         throw err;
     }
+
+    const data = updateLandingData(ctx, {}, update);
 
     const collection = ctx.db.collection(config.dbCollectionName);
     try {
