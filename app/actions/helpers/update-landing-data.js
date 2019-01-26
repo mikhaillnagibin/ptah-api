29e8f181c525ae4878f4ccf6af429019f8e0e031
Eugene Strigo <e.strigo@onlinepay.com>
2019-01-26 20:13:28 +0300

    PTH-211 #time 2h30m #comment Tests, part 1
    Tests infrastructure created
    Tests for update-landing-data helper written

--- app/actions/helpers/update-landing-data.js
+++ app/actions/helpers/update-landing-data.js
@@ -8,12 +8,17 @@ module.exports = (ctx, currentData, updatedData) => {
     currentData = currentData || {};
     updatedData = updatedData || {};
 
+    const userId = _.get(ctx, 'state.user.id');
+    if (!userId) {
+        throw new Error('User not created');
+    }
+
     const now = (new Date).toISOString();
 
     const defaults = {
         _id: mongo.ObjectId(),
         name: '',
-        userId: ctx.state.user.id,
+        userId: userId,
         createDate: now,
         updateDate: now,
         isPublished: false,
@@ -25,18 +30,22 @@ module.exports = (ctx, currentData, updatedData) => {
 
     const update = _.pick(updatedData, ['name', 'isPublished', 'domain', 'landing']);
 
-    let data = _.defaults(currentData, defaults);
+    let data = _.defaults(_.merge({}, currentData), defaults);
 
     if (update.isPublished === true && (data.isPublished !== update.isPublished || !updatedData.hasUnpublishedChanges)) {
         data.hasUnpublishedChanges = false;
     }
 
     if (update.landing && !_.isEqual(data.landing, update.landing)) {
-        data.currentVersion = data.currentVersion + 1;
+        ++data.currentVersion;
         data.hasUnpublishedChanges = true;
     }
 
-    data = _.merge(data, update);
+    if (data.currentVersion === 0) {
+        ++data.currentVersion;
+    }
+
+    data = _.merge({}, data, update);
 
     data.updateDate = now;
 
---
+++ test/fakes/fakes.js
@@ -0,0 +1,24 @@
+'use strict';
+
+const mongo = require('koa-mongo');
+
+const fakeLandingData = require('./landing');
+
+const fakeUserId = 'b62klDf0HeiJdNMv8K263nfE';
+const fakeId = '5c4c8b46125e69048cd1bd23';
+
+const fakeLanding = {
+    _id: mongo.ObjectId(fakeId),
+    name: 'My Landing Name',
+    userId: fakeUserId,
+    createDate: '2019-01-26T16:31:02.790Z',
+    updateDate: '2019-01-26T16:31:02.790Z',
+    isPublished: false,
+    hasUnpublishedChanges: false,
+    domain: '',
+    currentVersion: 1,
+    landing: fakeLandingData
+};
+
+module.exports.fakeUserId = fakeUserId;
+module.exports.fakeLanding = fakeLanding;
